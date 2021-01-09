9449c551eb98df2970361a2dd64913126249381b
theleand3000 <theleand3000@gmail.com>
2021-01-09 01:23:51 -0400

    Changes in crearUsuario.js

--- src/models/crearUsuario.js
+++ src/models/crearUsuario.js
@@ -11,29 +11,4 @@ var Usuario = function Usuario(usuario) {
   this.NumeroTelefono = usuario.NumeroTelefono;
 };
 
-Usuario.findByEmail = (Correo, result) => {
-  const request = sql.request();
-  request.input("Correo", Correo);
-  request.query("Select * from Usuario where Correo = @Correo", (err, res) => {
-    if (err) {
-      result(err, null);
-    }
-    result(null, res.recordset[0]);
-  });
-};
-
-Usuario.getUserById = (id, result) => {
-  const request = sql.request();
-  request.input("idUsuario", id);
-  request.query(
-    "Select * from Usuario where idUsuario = @idUsuario",
-    (err, res) => {
-      if (err) {
-        result(err, null);
-      }
-      result(null, res.recordset[0]);
-    }
-  );
-};
-
 module.exports = Usuario;
