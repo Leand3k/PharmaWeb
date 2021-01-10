d8ac15a8035b57d2da9b271190119432c314f0c0
theleand3000 <theleand3000@gmail.com>
2021-01-10 17:15:20 -0400

    Revert "Creacion de modelo de Articulos(falta testear) y creacion de carpeta config"
    This reverts commit 7c03e409053092976112ba6c1cb85ff7da5d5dd2.

--- src/Controllers/articulo.controller.js
+++ src/Controllers/articulo.controller.js
@@ -1,13 +1,6 @@
-const ArticuloModel = require('../models/obtenerArticulo')
 
 const getArticulos = (req, res) => {
-    ArticuloModel.GetArticlesList((err, articuloos) =>{
-        console.log('prueba2');
-        if(err)
-        res.send(err);
-        console.log('articulos', articuloos);
-        res.send(articuloos);
-    })
+    console.log('prueba');
 }
 
 module.exports = {getArticulos};
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const app = express();
 
 const port = process.env.RDS_PORT;
 
-const articlesRoutes = require('./Routes/articulo.routes');
+const articuloRoutes = require('./Routes/articulo.routes');
 
 app.get('/', (req, res) =>{
   res.send('hello world');
 });
 
-app.use('/api/v1/Articulo', articlesRoutes);
+app.use('/api/v1/Articulo', articuloRoutes);
 app.listen(3000, () => console.log(`Server running on port ${3000}`));
