9b11adb2811bc45bb08f54903aefa42075b88cd2
brianf.baez8@gmail.com <brianf.baez8@gmail.com>
2021-01-14 17:52:52 -0400

    fixed test

--- src/tests/testChaiHTTP.js
+++ src/tests/testChaiHTTP.js
@@ -1,21 +1,23 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-const expect = require('chai').expect;
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+const expect = require("chai").expect;
+const app = require("../index");
 
 chai.use(chaiHttp);
-const url= 'http://localhost:3000';
+// const url = "http://localhost:3000";
 
 /*
-* Test the GET articulo route
-*/
-describe('get all articulos: ',()=>{
-    it('should get all articulos', (done) => {
-    chai.request(url)
-    .get('/articulo/get-all')
-    .end( function(err,res){
-    console.log(res.body);
-    expect(res).to.have.status(200);
-    done();
-    });
-    });
-   });
+ * Test the GET articulo route
+ */
+describe("get all articulos: ", () => {
+  it("should get all articulos", (done) => {
+    chai
+      .request(app)
+      .get("/articulo/get-all")
+      .end(function (err, res) {
+        console.log(res.body);
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
